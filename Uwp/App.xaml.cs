e323c3cebbf687fffdbda8f3926c5cb3e70a1812
新田光宏 <potimarimo@gmail.com>
2020-04-05 22:06:03 +0900

    基本部分のみ

---
+++ Uwp/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+
+using Prism.Mvvm;
+using Prism.Unity.Windows;
+using Prism.Windows.AppModel;
+
+using Marimo.MappingGames.Uwp.Services;
+
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Marimo.MappingGames.Uwp
+{
+    [Windows.UI.Xaml.Data.Bindable]
+    public sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            // register a singleton using Container.RegisterType<IInterface, Type>(new ContainerControlledLifetimeManager());
+            base.ConfigureContainer();
+            Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
+        }
+
+        protected override async Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            await LaunchApplicationAsync(PageTokens.MainPage, null);
+        }
+
+        private async Task LaunchApplicationAsync(string page, object launchParam)
+        {
+            await ThemeSelectorService.SetRequestedThemeAsync();
+            NavigationService.Navigate(page, launchParam);
+            Window.Current.Activate();
+        }
+
+        protected override async Task OnActivateApplicationAsync(IActivatedEventArgs args)
+        {
+            await Task.CompletedTask;
+        }
+
+        protected override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            await base.OnInitializeAsync(args);
+            await ThemeSelectorService.InitializeAsync().ConfigureAwait(false);
+            Clipboard.SetContent(new DataPackage());
+            // We are remapping the default ViewNamePage and ViewNamePageViewModel naming to ViewNamePage and ViewNameViewModel to
+            // gain better code reuse with other frameworks and pages within Windows Template Studio
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(CultureInfo.InvariantCulture, "Marimo.MappingGames.Uwp.ViewModels.{0}ViewModel, Marimo.MappingGames.Uwp", viewType.Name.Substring(0, viewType.Name.Length - 4));
+                return Type.GetType(viewModelTypeName);
+            });
+        }
+
+        protected override IDeviceGestureService OnCreateDeviceGestureService()
+        {
+            var service = base.OnCreateDeviceGestureService();
+            service.UseTitleBarBackButton = false;
+            return service;
+        }
+    }
+}
---
+++ Uwp/ViewModels/MainViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Marimo.MappingGames.Uwp.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+
+        WriteableBitmap _元画像 = null;
+        public WriteableBitmap 元画像
+        {
+            get => _元画像;
+            set => SetProperty(ref _元画像, value);
+        }
+
+        public ObservableCollection<ImageSource> 分割された画像 { get; } = new ObservableCollection<ImageSource>();
+
+        ICommand _画像を取り込むCommand;
+        public ICommand 画像を取り込むCommand
+        {
+            get
+            {
+                if (_画像を取り込むCommand == null)
+                {
+                    _画像を取り込むCommand = new DelegateCommand<object>(
+                        async param =>
+                        {
+                            var c = Clipboard.GetContent();
+
+                            var b = await c.GetBitmapAsync();
+
+                            var stream = await b.OpenReadAsync();
+                            stream.AsStreamForWrite();
+                            var writeable = new WriteableBitmap(1, 1);
+                            await writeable.SetSourceAsync(stream);
+
+                            元画像 = writeable;
+                        });
+                }
+
+                return _画像を取り込むCommand;
+            }
+        }
+        ICommand _画像を分割するCommand;
+        public ICommand 画像を分割するCommand
+        {
+            get
+            {
+                if (_画像を分割するCommand == null)
+                {
+                    _画像を分割するCommand = new DelegateCommand<object>(
+                        param =>
+                        {
+
+                            var blockWidth = 32;
+                            var blockHeight = 32;
+
+                            var buffer = new byte[blockWidth * blockHeight * 4];
+                            元画像.PixelBuffer.ToArray();
+                            var a = 元画像.PixelBuffer.ToArray();
+                            for (var offsetWidth = 14; offsetWidth < 元画像.PixelWidth - blockWidth; offsetWidth += blockWidth)
+                            {
+                                for (var offsetHeight = 90; offsetHeight < 元画像.PixelHeight - blockHeight; offsetHeight += blockHeight)
+                                {
+                                    for (int i = 0; i < blockHeight; i++)
+                                    {
+
+                                        Array.Copy(
+                                            a,
+                                            (offsetWidth + (i + offsetHeight) * 元画像.PixelWidth) * 4,
+                                            buffer,
+                                            i * blockWidth * 4,
+                                            blockWidth * 4);
+                                    }
+                                    var block = new WriteableBitmap(blockWidth, blockHeight);
+                                    buffer.CopyTo(block.PixelBuffer);
+                                    分割された画像.Add(block);
+                                }
+                            }
+                        });
+                }
+
+                return _画像を分割するCommand;
+            }
+        }
+
+        
+    }
+}
